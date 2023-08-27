18751ab32882eacf6ee3338819c259648a41301f
aloneman13d <aloneman13d@gmail.com>
2023-08-27 20:19:23 +0700

    add Tailwind CSS

--- client/src/layouts/LandingLayout.vue
+++ client/src/layouts/LandingLayout.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <nav>
-      <router-link to="/login">Login</router-link>
+      <router-link to="/login" class="text-4xl mr-2">Login</router-link>
       <router-link to="/register">Register</router-link>
     </nav>
     <router-view />
@@ -15,5 +15,5 @@ export default {
 </script>
 
 <style>
-
+  
 </style>
