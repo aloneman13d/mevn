139fd6261d167f2e23d5ab1636fa2db6031f118b
aloneman13d <aloneman13d@gmail.com>
2023-08-29 00:25:30 +0700

    tailwind login page

--- client/src/views/Landing/IndexView.vue
+++ client/src/views/Landing/IndexView.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="grid gap-8 bg-slate-400 md:grid-cols-2 md:items-center w-full p-5 overflow-auto max-h-screen ">
-    <div class=" bg-white text-center p-8 rounded-md drop-shadow-sm">
+  <div class="grid gap-8 bg-slate-100 md:grid-cols-2 md:items-center w-full p-5 overflow-auto max-h-screen ">
+    <div class=" bg-white text-center p-8 rounded-md drop-shadow-2xl">
         <h1 class="text-3xl mb-2 text-sky-500">ระบบอะไรสักอย่าง</h1>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam nisi consequatur perferendis minus, nihil tempore excepturi voluptas deserunt debitis pariatur aut. Alias excepturi nemo assumenda? Odio rerum minima sunt nobis?</p>
         <nav class="mt-8">
-          <router-link to="/register" class="bg-slate-300 drop-shadow-sm hover:bg-sky-500 mx-3 my-4 px-3 py-1 rounded-md hover:text-slate-200">สมัครสมาชิก</router-link>
-          <router-link to="/login" class="bg-slate-300 drop-shadow-sm hover:bg-sky-500 mx-3 my-4 px-3 py-1 rounded-md hover:text-slate-200">ลงชื่อเข้าใช้</router-link>
+          <router-link to="/register" class="bg-slate-300 shadow-slate-600 shadow-md hover:bg-sky-500 mx-3 my-4 px-3 py-1 rounded-md hover:text-slate-200">สมัครสมาชิก</router-link>
+          <router-link to="/login" class="bg-slate-300 shadow-slate-600 shadow-md  hover:bg-sky-500 mx-3 my-4 px-3 py-1 rounded-md hover:text-slate-200">ลงชื่อเข้าใช้</router-link>
           
         </nav>
     </div>
--- client/src/views/Landing/LoginView.vue
+++ client/src/views/Landing/LoginView.vue
@@ -1,15 +1,69 @@
 <template>
-  <div>
-    <h3>Login</h3>
-  </div>
+  <session class="">
+    <form class="login-form">
+      <div class="grid content-center max-h-full h-screen">
+        <div class="grid content-center md:grid-cols-2 w-9/12 border-white border mx-auto rounded-lg">
+          <div class="hidden md:flex items-center bg-white">
+            <img class=" max-w-full md:rounded-l-lg "
+              src="https://img.freepik.com/free-vector/access-control-system-abstract-concept_335657-3180.jpg?t=st=1693219925~exp=1693220525~hmac=c3490d72c676b1af0fbd3852d6abf8ae4c288e6082dc9da12b564ac8d1590ca0"
+              alt="" />
+          </div>
+          <div class="py-20 px-10 bg-white">
+            <h1 class="text-sky-500 text-center text-2xl mb-8">Wecome</h1>
+
+            <div class="relative text-gray-600 focus-within:text-gray-400">
+              <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+                <svg stroke="currentColor" class="w-4 h-4 fill-slate-400" viewBox="0 0 1920 1920"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M0 1694.235h1920V226H0v1468.235ZM112.941 376.664V338.94H1807.06v37.723L960 1111.233l-847.059-734.57ZM1807.06 526.198v950.513l-351.134-438.89-88.32 70.475 378.353 472.998H174.042l378.353-472.998-88.32-70.475-351.134 438.89V526.198L960 1260.768l847.059-734.57Z"
+                    fill-rule="evenodd" />
+                </svg>
+              </span>
+              <input type="text"
+                class="mt-2 py-2 pl-10 rounded-none text-sm border-b border-gray-300 p-2.5 block w-full focus:outline-none focus:bg-white focus:text-gray-900"
+                placeholder="Email" />
+            </div>
+            <div class="relative text-gray-600 focus-within:text-gray-400">
+              <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+                <svg class="w-5]4 h-4 fill-none stroke-slate-400 stroke-2" viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M7 10.0288C7.47142 10 8.05259 10 8.8 10H15.2C15.9474 10 16.5286 10 17 10.0288M7 10.0288C6.41168 10.0647 5.99429 10.1455 5.63803 10.327C5.07354 10.6146 4.6146 11.0735 4.32698 11.638C4 12.2798 4 13.1198 4 14.8V16.2C4 17.8802 4 18.7202 4.32698 19.362C4.6146 19.9265 5.07354 20.3854 5.63803 20.673C6.27976 21 7.11984 21 8.8 21H15.2C16.8802 21 17.7202 21 18.362 20.673C18.9265 20.3854 19.3854 19.9265 19.673 19.362C20 18.7202 20 17.8802 20 16.2V14.8C20 13.1198 20 12.2798 19.673 11.638C19.3854 11.0735 18.9265 10.6146 18.362 10.327C18.0057 10.1455 17.5883 10.0647 17 10.0288M7 10.0288V8C7 5.23858 9.23858 3 12 3C14.7614 3 17 5.23858 17 8V10.0288"
+                    stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+              </span>
+              <input type="password"
+                class="mt-2 py-2 pl-10 rounded-none text-sm border-b border-gray-300 p-2.5 block w-full focus:outline-none focus:bg-white focus:text-gray-900"
+                placeholder="Password" />
+            </div>
+            <p class="text-xs text-sky-500 text-right">Forget password?</p>
+            <input type="button" value="Login"
+              class="rounded-2xl py-1 block bg-slate-800 text-white my-8 w-11/12 mx-auto" />
+            <div class="flex items-center py-4">
+              <!-- The left line -->
+              <div class="flex-grow h-px bg-gray-300"></div>
+
+              <!-- Your text here -->
+              <span class="flex-shrink text-xs text-gray-400 px-4 italic font-light">or</span>
+
+              <!-- The right line -->
+              <div class="flex-grow h-px bg-gray-300"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
+
+  </session>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style lang="sass" scoped>
-
+<style>
+body {
+  background-color: darkgrey;
+}
 </style>
